e35e719074adcfce29cd658d018eeeebdfe72741
Bilal Abdurahman <billa.abdu@gmail.com>
2020-12-05 08:32:52 -0700

    able to view quotes and click to see an individual quote

--- frontend/wetbatfront/src/router/index.js
+++ frontend/wetbatfront/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import QuotePage from "../views/QuotePage.vue";
 
+
 Vue.use(VueRouter);
 
 const routes = [
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "quotepage",
     component: QuotePage
+  },
+  {
+    path: "/quote/:id",
+    name: "ViewSingleQuote",
+    component: () => import("../views/SingleQuote.vue"),
   },
   
 ];
---
+++ frontend/wetbatfront/src/views/SingleQuote.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+<view-single-quote></view-single-quote>
+    </div>
+</template>
+
+<script>
+import ViewSingleQuote from '../components/ViewSingleQuote.vue'
+    export default {
+        name:"single-quote",
+
+        components: {
+                ViewSingleQuote,
+        },
+
+    }
+    
+</script>
+
+<style lang="scss" scoped>
+
+</style>
